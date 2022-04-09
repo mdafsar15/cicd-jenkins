ac80655d7e5a2bb5c2552ecaf0727ced771320ee
afsar.ansari <afsaralam7200@gmail.com>
2022-04-09 14:38:01 +0530

    cicd jenkins

---
+++ src/main/java/com/genx/CicdJenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.genx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdJenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CicdJenkinsApplication.class, args);
+	}
+
+}
