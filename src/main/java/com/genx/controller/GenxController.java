a4b489276095dfee957b833166728472ec3535ef
afsar.ansari <afsaralam7200@gmail.com>
2022-05-17 12:23:19 +0530

    added testCicd

--- src/main/java/com/genx/controller/GenxController.java
+++ src/main/java/com/genx/controller/GenxController.java
@@ -2,15 +2,35 @@ package com.genx.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.genx.model.Genx;
 
 @RestController
 @RequestMapping("/genx")
 public class GenxController {
-	
+
 	@GetMapping("/")
 	public String get() {
 		return "welcome to genx";
+	}
+
+	@GetMapping("/nexus")
+	public String nex() {
+		return "Congratulation nexus is working fine";
+	}
+
+	@RequestMapping(value = "/employee", method = RequestMethod.GET)
+	public Genx firstPage() {
+
+		Genx emp = new Genx();
+		emp.setName("emp1");
+		emp.setDesignation("manager");
+		emp.setEmpId("1");
+		emp.setSalary(3000);
+
+		return emp;
 	}
 
 }
--- src/test/java/com/genx/CicdJenkinsApplicationTests.java
+++ src/test/java/com/genx/CicdJenkinsApplicationTests.java
@@ -10,4 +10,5 @@ class CicdJenkinsApplicationTests {
 	void contextLoads() {
 	}
 
+	
 }
